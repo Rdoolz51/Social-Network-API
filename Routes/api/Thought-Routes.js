4f60f0447974a5c413a32d38ec28c34c4928e039
Rdoolz51 <Rdoolz51@aol.com>
2022-10-23 12:17:18 -0400

    added mongoose setup for proj

--- Routes/api/Thought-Routes.js
+++ Routes/api/Thought-Routes.js
@@ -1,1 +1,21 @@
 const router = require('express').Router();
+
+// /api/thoughts
+router.route('/')
+    .get(getThought)
+    .post(createThought);
+
+// /api/thoughts/:id
+router.route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+    .post(addReaction);
+
+
+router.route('/:thoughtId/reactions/:reactionId')
+    .put(deleteReaction);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,3 +9,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require("./routes"));
+
+//setup mongoose 
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/Social-Network-API', {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () =>
+    console.log(`connected at http://localhost:${PORT}`)
+);
